ce30758ce4982accbba9b58bd3510f47d27322e7
samandarolov1 <samandarolov1@gmail.com>
2024-02-26 19:07:55 +0300

    search ishladi

--- src/Opening/Navbar/SearchPanel.jsx
+++ src/Opening/Navbar/SearchPanel.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import "./Navbar.css"
+import { BsTruck } from "react-icons/bs";
+
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 export default function SearchPanel() {
+    const navigateF = useNavigate()
     const { searchItems } = useSelector(state => state.main)
     return (
         <div className='searchPanel'>
             <span></span>
-            <div>
+            <div className='searchCardOrovchi'>
                 {
                     searchItems.map(i => (
-                        <div key={i.id}>{i.title}</div>
+                        <div className='searchCards' key={i.id} onClick={() => navigateF(`view/${i.id}`)}>
+                            <figure><img src={i.img} alt={i.title.slice(0, 19) + "..."} /></figure>
+                            <div className='searchAddition'>
+                                <p>{i.title}</p>
+                                <div className="deliver-truck"> <div><BsTruck /></div>Buyurtma orqali</div>
+                                <div className='searchPrice'>{i.price} $</div>
+                            </div>
+                        </div>
                     ))
                 }
             </div>
